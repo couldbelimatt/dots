97904041a26ed8d4243eac27519344301143cfe2
Matt P <77085019+couldbelimatt@users.noreply.github.com>
2024-09-23 14:39:49 +0100

    Update rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -315,7 +315,7 @@ globalkeys = gears.table.join(
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
-    	 awful.key({ }, "Print", function() awful.spawn("xfce4-screenshooter") end,
+    	 awful.key({ }, "Print", function() awful.spawn("gnome-screenshot -ca") end,
 	 			{description = "take a screenshot", group = "launcher"}),
 
 	    awful.key({ modkey }, "x", function () awful.spawn("betterlockscreen -l") end,
